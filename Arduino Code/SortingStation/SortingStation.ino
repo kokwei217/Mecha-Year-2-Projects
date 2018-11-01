006315633b23ccc1ec9097ec940c74f782cf872c
kokwei217 <40696840+kokwei217@users.noreply.github.com>
2018-11-01 18:56:29 +0800

    Integrated XY's part 3  into main code

--- Arduino Code/SortingStation/SortingStation.ino
+++ Arduino Code/SortingStation/SortingStation.ino
@@ -1,15 +1,19 @@
 // 2, 3, 18, 19, 20, 21  = Interrupt Pin
 //Inputs
 int metalSensor = 2;
-int sortSensor = 3; //IR sensor
+int sortSensor = 3; //IR sensor 1st
 int hopperSensor = 4;
+int in5 = 53; // IR sensor reject
+int in3 = 11; //capacitive sensor
 int hopperState = LOW;
+int Sin3 = 1;
 
 //OUTPUTS
 int belt_1 = 5;
 int belt_2 = 6;
 int sortSolenoid = 8;
 int rotarySolenoid = 7;
+int rejectSolenoid = 9;
 
 //boolean control and timing interval
 bool flag_pass = false;
@@ -25,12 +29,16 @@ unsigned long currentTime;
 unsigned long fp_latchedTime = 0;
 unsigned long t_out1 = 0;
 unsigned long t_out2 = 0;
+unsigned long t_out3 = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(metalSensor, INPUT_PULLUP);
   pinMode(sortSensor, INPUT_PULLUP);
   pinMode(hopperSensor, INPUT);
+  pinMode(in5 , INPUT);
+  pinMode(in3 , INPUT);
+  pinMode(rejectSolenoid, OUTPUT);
   pinMode(sortSolenoid, OUTPUT);
   pinMode(rotarySolenoid, OUTPUT);
   pinMode(belt_1 , OUTPUT);
@@ -52,16 +60,50 @@ void loop() {
     flag_counterN = true;
     t_out2 = currentTime;
   }
+
+  //Xing Yik's Part
+  Serial.print(digitalRead(in3));
+  Serial.print(Sin3);
+  Serial.print(digitalRead(in5));
+  Serial.print("\t");
+  Serial.print(millis());
+  Serial.print("\t");
+  Serial.println(t_out3);
+  
+  if (digitalRead(in3) == 0)
+  {
+    Sin3 = 0;
+  }
+
+  if (digitalRead(in5) == HIGH)
+  {
+    if (Sin3 == 1)// sensed metal & solenoid
+    {
+      digitalWrite(rejectSolenoid, HIGH);
+      t_out3 = millis();
+    }
+    else
+    {
+      t_out3 = millis();
+      Serial.println("assembly");
+      while (millis() - t_out3 < 1000)
+      {
+        //nothing
+      }
+      Sin3 = 1;
+    }
+  }
+
 }
 
 void metalDetected() {
-//  Serial.println("metal");
+  //  Serial.println("metal");
   flag_pass = true;
   fp_latchedTime = currentTime;
 }
 
 void componentDetected_1st () {
-//  Serial.println("push");
+  //  Serial.println("push");
   if (!flag_pass && queueCounter < 5) {
     digitalWrite(sortSolenoid, HIGH);
     t_out1 = currentTime;
@@ -93,6 +135,12 @@ void timingControl() {
     digitalWrite( rotarySolenoid, LOW);
     queueCounter--;
     flag_counterN = false;
+  }
+
+  
+  if (currentTime - t_out3 > extendPeriod)
+  {
+    digitalWrite(rejectSolenoid, LOW);
   }
 }
 
