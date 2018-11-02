47d266c77de4721f670de66470e6e3ed5e47b186
kokwei217 <40696840+kokwei217@users.noreply.github.com>
2018-11-02 12:03:29 +0800

    fixed bugs and completed sorting station code

--- Arduino Code/SortingStation/SortingStation.ino
+++ Arduino Code/SortingStation/SortingStation.ino
@@ -1,12 +1,12 @@
 // 2, 3, 18, 19, 20, 21  = Interrupt Pin
 //Inputs
 int metalSensor = 2;
-int sortSensor = 3; //IR sensor 1st
+int sortSensor = 3;
 int hopperSensor = 4;
-int in5 = 53; // IR sensor reject
-int in3 = 11; //capacitive sensor
+int in5 = 18;
+int in4 = 53;
+int in3 = 11;
 int hopperState = LOW;
-int Sin3 = 1;
 
 //OUTPUTS
 int belt_1 = 5;
@@ -16,27 +16,35 @@ int rotarySolenoid = 7;
 int rejectSolenoid = 9;
 
 //boolean control and timing interval
-bool flag_pass = false;
+bool flag_metal = false;
 bool flag_counter = true; //Counter++
 bool flag_counterN = false; //Counter--
 bool flag_rotate = true;
-int fp_interval = 2500; //flag_pass interval
+bool flag_check = false;
+bool flag_reject = false;
+bool flag_pass = true;
+int fm_interval = 2500; //flag_metal interval
 int extendPeriod = 300; //output extension time
-int rotationDelay = 2200;
+int rotationDelay = 1100;
 int queueCounter;
 
 unsigned long currentTime;
 unsigned long fp_latchedTime = 0;
+unsigned long passTime = 0; //time when assembled product pass through ip 5
 unsigned long t_out1 = 0;
 unsigned long t_out2 = 0;
 unsigned long t_out3 = 0;
+unsigned long t_ip4 = 0;
+unsigned long t_ip3 = 0;
+
 
 void setup() {
   Serial.begin(9600);
   pinMode(metalSensor, INPUT_PULLUP);
   pinMode(sortSensor, INPUT_PULLUP);
   pinMode(hopperSensor, INPUT);
-  pinMode(in5 , INPUT);
+  pinMode(in5 , INPUT_PULLUP);
+  pinMode(in4, INPUT);
   pinMode(in3 , INPUT);
   pinMode(rejectSolenoid, OUTPUT);
   pinMode(sortSolenoid, OUTPUT);
@@ -44,14 +52,15 @@ void setup() {
   pinMode(belt_1 , OUTPUT);
   pinMode(belt_2 , OUTPUT);
   attachInterrupt(digitalPinToInterrupt(metalSensor), metalDetected, RISING);
-  attachInterrupt(digitalPinToInterrupt(sortSensor), componentDetected_1st, HIGH);
+  attachInterrupt(digitalPinToInterrupt(sortSensor), componentDetected_1st, RISING);
+  attachInterrupt(digitalPinToInterrupt(in5), rejectionCheck, RISING);
   digitalWrite(belt_1, HIGH);
   digitalWrite(belt_2, HIGH);
   queueCounter  = 0;
 }
 
 void loop() {
-  Serial.println (queueCounter);
+  Serial.print (queueCounter);
   currentTime = millis();
   timingControl();
   hopperState = digitalRead(hopperSensor);
@@ -62,49 +71,32 @@ void loop() {
   }
 
   //Xing Yik's Part
-  Serial.print(digitalRead(in3));
-  Serial.print(Sin3);
-  Serial.print(digitalRead(in5));
-  Serial.print("\t");
-  Serial.print(millis());
-  Serial.print("\t");
-  Serial.println(t_out3);
-  
-  if (digitalRead(in3) == 0)
-  {
-    Sin3 = 0;
+  Serial.print(flag_pass);
+  Serial.println(flag_reject);
+
+  //detect assembled
+  if (digitalRead(in3) == LOW)  {
+    flag_pass = true;
+    //    flag_reject = false;
+    t_ip3 = currentTime;
   }
 
-  if (digitalRead(in5) == HIGH)
-  {
-    if (Sin3 == 1)// sensed metal & solenoid
-    {
-      digitalWrite(rejectSolenoid, HIGH);
-      t_out3 = millis();
-    }
-    else
-    {
-      t_out3 = millis();
-      Serial.println("assembly");
-      while (millis() - t_out3 < 1000)
-      {
-        //nothing
-      }
-      Sin3 = 1;
-    }
+  //detect Others
+  if (digitalRead(in4) == LOW && !flag_pass) {
+    Serial.println("REJECT");
+    flag_check = true;
+    t_ip4 = currentTime;
   }
-
 }
 
+
 void metalDetected() {
-  //  Serial.println("metal");
-  flag_pass = true;
+  flag_metal = true;
   fp_latchedTime = currentTime;
 }
 
 void componentDetected_1st () {
-  //  Serial.println("push");
-  if (!flag_pass && queueCounter < 5) {
+  if (!flag_metal && queueCounter < 5) {
     digitalWrite(sortSolenoid, HIGH);
     t_out1 = currentTime;
     flag_rotate = false;
@@ -115,10 +107,18 @@ void componentDetected_1st () {
   }
 }
 
+void rejectionCheck () {
+  if (flag_reject ) {
+    digitalWrite(rejectSolenoid, HIGH);
+    t_out3 = currentTime;
+    flag_reject = false;
+  }
+}
+
 void timingControl() {
   //Control passing of metal, unflag after the interval;
-  if ( (currentTime - fp_latchedTime) > fp_interval) {
-    flag_pass = false;
+  if ( (currentTime - fp_latchedTime) > fm_interval) {
+    flag_metal = false;
   }
 
   //Control extension of solenoid, retract after extendPeriod
@@ -137,10 +137,18 @@ void timingControl() {
     flag_counterN = false;
   }
 
-  
-  if (currentTime - t_out3 > extendPeriod)
-  {
+  if (currentTime - t_out3 > extendPeriod) {
     digitalWrite(rejectSolenoid, LOW);
+  }
+
+  //flag reject afteer 1.8sec
+  if ((currentTime - t_ip4) > 1800 && flag_check) {
+    flag_check = false;
+    flag_reject = true;
+  }
+
+  if ((currentTime - t_ip3) > 1500) {
+    flag_pass = false;
   }
 }
 
